37a15a5e750fe546b3dfc7c65aa6c2a40868a0c0
yaduit <yk226411@gmail.com>
2025-12-10 22:58:19 +0530

    frontend added axios , implemented routes react router dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
+      <Route path='/books/details/:id' element={<ShowBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+   </Routes>
+  )
+}
